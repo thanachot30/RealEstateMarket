3e451bb0751d2de77a49f06bb70a7be460d813b5
thanachot30 <su.thanachot@gmail.com>
2023-11-06 16:08:14 +0700

    create page and rount

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Signin from './pages/Signin.jsx'
+import Signout from './pages/Signout.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 export default function App() {
   return (
-    <div>
-      <h1 className='text-4xl font-bold underline text-red-200'>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
