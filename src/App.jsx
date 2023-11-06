53440bfa57c43f1cbe23963d2ab23cab68d66dd2
thanachot30 <su.thanachot@gmail.com>
2023-11-06 17:18:57 +0700

    create nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Signin from './pages/Signin.jsx'
 import Signout from './pages/Signout.jsx'
 import About from './pages/About.jsx'
 import Profile from './pages/Profile.jsx'
+import Header from "./components/Header.jsx"
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
