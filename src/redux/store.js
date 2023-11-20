d2c51d105119a1347138c06b41d5ca2fdfc7759d
thanachot30 <su.thanachot@gmail.com>
2023-11-20 16:21:14 +0700

    add redux persis

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import useReducer from './user/userSlic.js'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer,persistStore} from 'redux-persist'
+
+const rootReducer = combineReducers({user: useReducer});
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version:1
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: useReducer
-},
+  reducer:persistedReducer,
 })
+
+export const persistor = persistStore(store);
+
